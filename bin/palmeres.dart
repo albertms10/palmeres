1a7156d3aff0c63f349561c475abb9f32c7af7e0
Albert Mañosa <26429103+albertms10@users.noreply.github.com>
2024-06-01 09:55:39 +0200

    feat(palmeres): 🥅 throw `ArgumentError` on invalid `seed`
    Signed-off-by: Albert Mañosa <26429103+albertms10@users.noreply.github.com>

--- bin/palmeres.dart
+++ bin/palmeres.dart
@@ -73,6 +73,16 @@ final parser = ArgParser()
 Future<void> main(List<String> arguments) async {
   final results = parser.parse(arguments);
   final seedArg = results.option(_seed);
+
+  final shuffle = results.flag(_shuffle);
+  if (!shuffle && seedArg != null) {
+    throw ArgumentError.value(
+      seedArg,
+      'seed',
+      'Use --shuffle to specify the seed',
+    );
+  }
+
   final schedule = results.multiOption(_person).toSet().allocateWeeks(
         from: DateTime.parse(results.option(_from)!),
         weeksPerPerson: int.parse(results.option(_weeksPerPerson)!),
