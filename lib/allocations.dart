ab52eaa6b43da1c3a6e7b3a71a708dd1d23f4b9f
Albert Mañosa <26429103+albertms10@users.noreply.github.com>
2024-06-01 12:47:58 +0200

    feat(palmeres): 🧑‍💻 allow overriding `headers` by argument
    Signed-off-by: Albert Mañosa <26429103+albertms10@users.noreply.github.com>

--- lib/allocations.dart
+++ lib/allocations.dart
@@ -6,17 +6,19 @@ final _yMd = DateFormat('yyyy-MM-dd');
 extension Allocations
     on List<(DateTime date, String apartment, String person)> {
   /// This record list formatted as TSV (tab-separated values).
-  String toTSV({
-    (String, String, String) headers = const ('Date', 'Apartment', 'Person'),
-  }) =>
-      (StringBuffer()
-            ..writeAll(
-              [
-                '${headers.$1}\t${headers.$2}\t${headers.$3}',
-                for (final item in this)
-                  '${_yMd.format(item.$1)}\t${item.$2}\t${item.$3}',
-              ],
-              '\n',
-            ))
-          .toString();
+  String toTSV({(String, String, String)? headers}) {
+    headers ??= const ('Date', 'Apartment', 'Person');
+
+    final buffer = StringBuffer()
+      ..writeAll(
+        [
+          '${headers.$1}\t${headers.$2}\t${headers.$3}',
+          for (final item in this)
+            '${_yMd.format(item.$1)}\t${item.$2}\t${item.$3}',
+        ],
+        '\n',
+      );
+
+    return buffer.toString();
+  }
 }
