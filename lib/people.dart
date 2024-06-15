eee1e8a18e8761e35ffdaa4e10bcc894ec069fc3
Albert Mañosa <26429103+albertms10@users.noreply.github.com>
2024-06-15 07:37:38 -0500

    refactor(people): ♻️ rename `allocateApartments` method (#16)
    Signed-off-by: Albert Mañosa <26429103+albertms10@users.noreply.github.com>

--- lib/people.dart
+++ lib/people.dart
@@ -12,53 +12,54 @@ typedef RowTuple = (DateTime date, Apartment apartment, Person person);
 
 /// A people extension.
 extension People on Iterable<Person> {
-  /// Allocates each person in this collection in weeks starting [from] a date,
+  /// Allocates each person in this collection starting [from] a date,
   /// given the number of [weeksPerPerson] and a list of [apartments].
   ///
   /// Use [shuffle] to randomize the people collection. Optionally, [seed]
   /// can be provided to initialize the internal state of the random generator.
-  List<RowTuple> allocateWeeks({
+  List<RowTuple> allocateApartments({
     required DateTime from,
-    int weeksPerPerson = 1,
     List<Apartment> apartments = const [Apartment('')],
+    int weeksPerPerson = 1,
     bool shuffle = false,
     int? seed,
   }) {
     assert(isNotEmpty, 'This collection must contain at least one item');
     assert(apartments.isNotEmpty, 'Apartments must contain at least one item');
 
-    final sortedPeople = toList();
+    final people = toList();
     if (shuffle) {
       final random = Random(seed ?? 0 * 1000 + from.year);
-      sortedPeople.shuffle(random);
+      people.shuffle(random);
     }
 
     return [
       for (var i = 0; i < apartments.length; i++)
-        ..._allocateWeeks(
-          people: sortedPeople,
-          from: from,
-          apartment: apartments[i],
-          weeksPerPerson: weeksPerPerson,
-          indexShift: length ~/ (i + 1),
-        ).recordEntries,
+        ...people
+            .allocateWeeks(
+              from: from,
+              weeksPerPerson: weeksPerPerson,
+              indexShift: length ~/ (i + 1),
+            )
+            .entries
+            .map((entry) => (entry.key, apartments[i], entry.value)),
     ]..sort(_compareToFirst);
   }
+}
 
-  static Map<DateTime, ({Apartment apartment, Person person})> _allocateWeeks({
-    required List<Person> people,
+extension on List<Person> {
+  Map<DateTime, Person> allocateWeeks({
     required DateTime from,
-    required Apartment apartment,
     int weeksPerPerson = 1,
     int indexShift = 0,
   }) {
-    final totalWeeks = weeksPerPerson * people.length;
-    final schedule = <DateTime, ({Apartment apartment, Person person})>{};
+    final totalWeeks = weeksPerPerson * length;
+    final schedule = <DateTime, Person>{};
 
     var runDate = from;
     for (var i = 0; i < totalWeeks; i++) {
-      final person = people[(i + indexShift) % people.length];
-      schedule[runDate] = (apartment: apartment, person: person);
+      final person = this[(i + indexShift) % length];
+      schedule[runDate] = person;
       runDate = runDate.add(_oneWeek);
     }
 
@@ -109,13 +110,6 @@ extension GroupedByDateSchedule on Map<DateTime, List<RowTuple>> {
     }
     print('==================\n');
   }
-}
-
-extension on Map<DateTime, ({Apartment apartment, Person person})> {
-  List<RowTuple> get recordEntries => [
-        for (final entry in entries)
-          (entry.key, entry.value.apartment, entry.value.person),
-      ];
 }
 
 Comparator<RowTuple> get _compareToFirst => (a, b) {
